506c69c8ea4aa4c17e388e6fa1ed7bfdd969b066
Ollie Weijermars <ollie@cookingfox.nl>
2017-04-30 20:07:50 +0200

    Optimized the raindrop effect. Now with layers

--- RainDropV1/RainDropV1.ino
+++ RainDropV1/RainDropV1.ino
@@ -8,7 +8,6 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(Pixels, PIN, NEO_GRB + NEO_KHZ800);
 float dimForce = 0.05;
 uint16_t rainMax = 100;
 uint16_t rainIntensity = 1;
-float fadeRate = 1.005;
 
 uint16_t halfPixels = Pixels * 0.5;
 //rainDropEdgeBegin 0  -
@@ -17,15 +16,14 @@ uint16_t rainDropEdgeEnd = halfPixels;
 
 uint16_t sun[3] = {252, 236, 60};
 uint16_t night[3] = {30, 30, 180};
+uint16_t day[3] = {110, 110, 255};
 
-uint16_t initialRed = night[0];
-uint16_t initialGreen = night[1];
-uint16_t initialBlue = night[2];
-
-uint16_t targetRed = 1 + floor(initialRed * dimForce);
-uint16_t targetGreen = 1 + floor(initialGreen * dimForce);
-uint16_t targetBlue = 1 + floor(initialBlue * dimForce);
-
+// Exist out of the temparature / day and night cicle
+uint16_t layer1RGB[Pixels][3];
+// Exist out  dim force
+float layer2RGB[Pixels];
+float initialDimForce = 0.2;
+float fadeRate = 0.0009;
 
 // windSpeed 0-9
 uint16_t windSpeed = 0;
@@ -37,18 +35,24 @@ float greenStates[Pixels];
 void setup() {
   strip.begin();
   strip.setBrightness(255);
-  // Initialize all pixels to 'off'
+  
+  // Initialize layer2RGB
   for (uint16_t l = 0; l < Pixels; l++) {
-    redStates[l] = initialRed;
-    greenStates[l] = initialGreen;
-    blueStates[l] = initialBlue;
+    layer2RGB[l] = 1.0;
   }
 
   strip.show();
-
 }
 
 void loop () {
+  // define first layer
+  for (uint16_t l = 0; l < Pixels; l++) {
+    layer1RGB[l][0] = day[0];
+    layer1RGB[l][1] = day[1];
+    layer1RGB[l][2] = day[2];
+    strip.setPixelColor(l, layer1RGB[l][0], layer1RGB[l][1], layer1RGB[l][2]);
+  }
+
   updateRain(windSpeed);
   strip.show();
 
@@ -59,53 +63,26 @@ void updateRain(uint16_t _windSpeed) {
   rainDropEdgeBegin += _windSpeed;
   rainDropEdgeEnd += _windSpeed;
   uint16_t margin = 40;
-  
+
   if (random(rainMax) <= rainIntensity) {
     uint16_t i = random(halfPixels);
     if (i >= rainDropEdgeBegin && i <= rainDropEdgeEnd)
       i += halfPixels ;
 
-    if (redStates[i] >= (initialRed - margin) && greenStates[i] >= (initialGreen - margin) && blueStates[i] >= (initialBlue - margin)) {
-      redStates[i] = targetRed;
-      greenStates[i] = targetGreen;
-      blueStates[i] = targetBlue;
+    if (layer2RGB[i] >= 1) {
+      layer2RGB[i] = initialDimForce;
     }
   }
 
   for (uint16_t l = 0; l < Pixels; l++) {
-    if (redStates[l] <= initialRed || greenStates[l] <= initialRed || blueStates[l] <= initialRed) {
-      strip.setPixelColor(l, redStates[l], greenStates[l], blueStates[l]);
-
-      if (redStates[l] < initialRed) {
-        redStates[l] = redStates[l] * fadeRate ;
-        if (redStates[l] > 255) {
-          redStates[l] = 255;
-        }
-      } else {
-        redStates[l] = initialRed;
-      }
-
-      if (greenStates[l] < initialGreen) {
-        greenStates[l] =  greenStates[l] * fadeRate ;
-        if (greenStates[l] > 255) {
-          greenStates[l] = 255;
-        }
-      } else {
-        greenStates[l] = initialGreen;
-      }
-
-      if (blueStates[l] < initialBlue) {
-        blueStates[l] =  blueStates[l] * fadeRate ;
-        if (blueStates[l] > 255) {
-          blueStates[l] = 255;
-        }
-      } else {
-        blueStates[l] = initialBlue;
-      }
-
-    } else {
-      strip.setPixelColor(l, initialRed, initialGreen, initialBlue);
+    if (layer2RGB[l] < 1) {
+    layer1RGB[l][0] *= layer2RGB[l];
+    layer1RGB[l][1] *= layer2RGB[l];
+    layer1RGB[l][2] *= layer2RGB[l];
+    
+    layer2RGB[l] += (layer2RGB[l] * 0.00001) + fadeRate;
+    
+      strip.setPixelColor(l, layer1RGB[l][0], layer1RGB[l][1], layer1RGB[l][2]);
     }
   }
 }
-
