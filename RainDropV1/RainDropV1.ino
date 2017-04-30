3b45a5bb0a5d01648a57a23b2e1b033a14db89f0
Ollie Weijermars <ollie@cookingfox.nl>
2017-04-30 02:13:58 +0200

    added top rain effect

--- RainDropV1/RainDropV1.ino
+++ RainDropV1/RainDropV1.ino
@@ -16,22 +16,24 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(Pixels, PIN, NEO_GRB + NEO_KHZ800);
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
 // and minimize distance between Arduino and first pixel.  Avoid connecting
 // on a live circuit...if you must, connect GND first.
-uint16_t dimForce = 10;
+float dimForce = 0.05;
 uint16_t rainMax = 100;
-uint16_t rainIntensity = 0;
+uint16_t rainIntensity = 1;
+float fadeRate = 1.005;
+
+
 
 uint16_t sun[3] = {252, 236, 60};
 uint16_t night[3] = {30, 30, 180};
 
-uint16_t initialRed = sun[0];
-uint16_t initialGreen = sun[1];
-uint16_t initialBlue = sun[2];
+uint16_t initialRed = night[0];
+uint16_t initialGreen = night[1];
+uint16_t initialBlue = night[2];
 
-uint16_t targetRed = 1 + floor(initialRed / dimForce);
-uint16_t targetGreen = 1 + floor(initialGreen / dimForce);
-uint16_t targetBlue = 1 + floor(initialBlue / dimForce);
+uint16_t targetRed = 1 + floor(initialRed * dimForce);
+uint16_t targetGreen = 1 + floor(initialGreen * dimForce);
+uint16_t targetBlue = 1 + floor(initialBlue * dimForce);
 
-float fadeRate = 1.003;
 
 float redStates[Pixels];
 float blueStates[Pixels];
@@ -41,18 +43,22 @@ float greenStates[Pixels];
 void setup() {
   strip.begin();
   strip.setBrightness(255);
-  strip.show(); // Initialize all pixels to 'off'
+   // Initialize all pixels to 'off'
   for (uint16_t l = 0; l < Pixels; l++) {
     redStates[l] = initialRed;
     greenStates[l] = initialGreen;
     blueStates[l] = initialBlue;
   }
+  strip.show();
 }
 
 void loop () {
-  uint16_t margin = 0;
+  uint16_t margin = 40;
   if (random(rainMax) <= rainIntensity) {
-    uint16_t i = random(Pixels);
+    uint16_t i = random((Pixels * 0.5));
+    if (i >= 10)
+      i += 18;
+
     if (redStates[i] >= (initialRed - margin) && greenStates[i] >= (initialGreen - margin) && blueStates[i] >= (initialBlue - margin)) {
       redStates[i] = targetRed;
       greenStates[i] = targetGreen;
@@ -75,8 +81,8 @@ void loop () {
 
       if (greenStates[l] < initialGreen) {
         greenStates[l] =  greenStates[l] * fadeRate ;
-        if(greenStates[l]>255){
-          greenStates[l]=255;
+        if (greenStates[l] > 255) {
+          greenStates[l] = 255;
         }
       } else {
         greenStates[l] = initialGreen;
@@ -84,8 +90,8 @@ void loop () {
 
       if (blueStates[l] < initialBlue) {
         blueStates[l] =  blueStates[l] * fadeRate ;
-        if(blueStates[l]>255){
-          blueStates[l]=255;
+        if (blueStates[l] > 255) {
+          blueStates[l] = 255;
         }
       } else {
         blueStates[l] = initialBlue;
